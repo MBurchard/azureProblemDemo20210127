46f71ee9926298394e1fe43731b633c2a510d446
Martin Burchard <dev@burchard-it.de>
2021-01-27 07:36:14 +0100

    add servicebus access, it is working

--- src/background.ts
+++ src/background.ts
@@ -3,6 +3,7 @@
 import { app, protocol, BrowserWindow } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+import { ServiceBusClient } from "@azure/service-bus";
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Scheme must be registered before the app is ready
@@ -11,6 +12,24 @@ protocol.registerSchemesAsPrivileged([
 ]);
 
 async function createWindow() {
+  const sbClient = new ServiceBusClient(
+    process.env.VUE_APP_AZURE_SERVICEBUS_CONNECTION_STRING || ""
+  );
+  const sender = sbClient.createSender(
+    process.env.VUA_APP_AZURE_SERVICEBUS_TOPICNAME || ""
+  );
+
+  await sender.sendMessages({
+    body: "hello world"
+  });
+
+  const receiver = sbClient.createReceiver(
+    process.env.VUA_APP_AZURE_SERVICEBUS_TOPICNAME || "",
+    process.env.VUA_APP_AZURE_SERVICEBUS_SUBSCRIPTION || ""
+  );
+  const messages = await receiver.receiveMessages(1);
+  console.log(messages);
+
   // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
@@ -30,7 +49,7 @@ async function createWindow() {
   } else {
     createProtocol("app");
     // Load the index.html when not in development
-    win.loadURL("app://./index.html");
+    await win.loadURL("app://./index.html");
   }
 }
 
@@ -43,10 +62,10 @@ app.on("window-all-closed", () => {
   }
 });
 
-app.on("activate", () => {
+app.on("activate", async () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  if (BrowserWindow.getAllWindows().length === 0) await createWindow();
 });
 
 // This method will be called when Electron has finished
@@ -61,7 +80,7 @@ app.on("ready", async () => {
       console.error("Vue Devtools failed to install:", e.toString());
     }
   }
-  createWindow();
+  await createWindow();
 });
 
 // Exit cleanly on request from parent process in development mode.
